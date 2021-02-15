9bf4f9ec173f1c41fad62f880f94bfaf0bf988fd
Erika Raymundo <zyferion@gmail.com>
2021-02-15 17:32:55 +1100

    Create removeDuplicates.py

---
+++ Python3/removeDuplicates.py
@@ -0,0 +1,24 @@
+# Problem 26
+#https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+
+def removeDuplicates(nums):
+    
+    # Initiate index
+    i = 0
+    
+    # Only loop until last element 
+    while (i < len(nums)-1):
+        
+        # Store element to check
+        elem = nums[i]
+        
+        # If element is same as next element, delete current element
+        if elem == nums[i+1]:
+            
+            del nums[i]
+        
+        # Else go to next element in list
+        else:
+            i = i+1
+        
+    return(len(nums))
